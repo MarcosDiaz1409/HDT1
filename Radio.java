e02f6bbada9cdfba8615b8b4f32d71a6a8c36824
CARPTatj <car22539@uvg.edu.gt>
2023-01-16 12:25:09 -0600

    Radio

--- Radio.java
+++ Radio.java
@@ -5,12 +5,70 @@
  * Clase case que implementa la interfaz: clase controladora, crea los métodos que se utilizarán en la principal.
  */
 public class Radio implements iRadio {
+    private String frequence;
+    private double FMActualStation;
+    private int AMActualStation;
+    private int[] slotAM;
+    private double[] slotFM;
+    private boolean encendido;
+
+    public int[] getSlotAM() {
+        return this.slotAM;
+    }
+
+    public void setSlotAM(int[] slotAM) {
+        this.slotAM = slotAM;
+    }
+
+    public double[] getSlotFM() {
+        return this.slotFM;
+    }
+
+    public void setSlotFM(double[] slotFM) {
+        this.slotFM = slotFM;
+    }
+
+    public boolean isEncendido() {
+        return this.encendido;
+    }
+
+    public boolean getEncendido() {
+        return this.encendido;
+    }
+
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
+
+    public Radio() {
+        frequence = "AM";
+        FMActualStation = 97.9;
+        AMActualStation = 540;
+        slotAM = new int [13];
+        slotFM = new double [13];
+        encendido = true;
+    }
+
+
+    public Radio(String frequence, double FMActualStation, int AMActualStation, int[] slotAM, double[] slotFM, boolean encendido) {
+        this.frequence = frequence;
+        this.FMActualStation = FMActualStation;
+        this.AMActualStation = AMActualStation;
+        this.slotAM = slotAM;
+        this.slotFM = slotFM;
+        this.encendido = encendido;
+    }
+
+
     public void on() {
         // TODO Auto-generated method stub
+        this.encendido = true;
     }
 
     public void off() {
         // TODO Auto-generated method stub
+        this.encendido = false;
     }
 
     public boolean isOn() {
